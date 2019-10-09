c80de7dc3a444a5b86bcf44a8704bfd7277f3207
leishuai <2258010965@qq.com>
2019-10-10 01:14:53 +0800

    优化代码

--- src/main/java/com/tl/git/A.java
+++ src/main/java/com/tl/git/A.java
@@ -4,11 +4,8 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("leishuai..........");
-		
 		System.out.println("leishuai");
-		
 	}
-	
 	public void testname() throws Exception {
 		System.out.println("leishuai");
 	}
